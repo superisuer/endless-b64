16c07f1d9947844837782bb24e216ff6922d7484
Den <superisuer@proton.me>
2025-08-22 15:15:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ def main():
         while 1:
             text = base64.b64encode(text.encode('utf-8')).decode('utf-8')
     except KeyboardInterrupt:
-        with open('encoded.txt', 'w', encoding='utf-8') as file:
-            file.write(text)
-        print(f"Encoded text writed in 'encoded.txt'. {len(text)} symbols.")
+        agr = input(f"Do you want to write {len(text)/1000000} Mb to the 'encoded.txt' file? (Y/n):\n").lower()
+        if agr == 'y':
+            with open('encoded.txt', 'w', encoding='utf-8') as file:
+                file.write(text)
+            print(f"Encoded text writed in 'encoded.txt'. {len(text)} bytes.")
+    
 
 if __name__ == "__main__":
     main()
